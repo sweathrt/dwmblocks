4075c81c76222248d846fbb999361abaaf09f8dc
sweathrt <sweathrt.d@gmail.com>
2022-08-20 19:04:20 +0300

    Initial commit

---
+++ config.h
@@ -0,0 +1,14 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{" ",           "sb-memory",							10,		6},
+	{"",            "sb-volume",							 0,		3},
+	{"",		"sb-internet",							 5,		2},
+	{"",            "sb-time",							60,		4},
+	{"", 		"sb-date",						     86400,		5},
+	{"", 		"sb-xkb-layout",						 0,		1},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
